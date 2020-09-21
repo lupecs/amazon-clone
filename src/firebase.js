b0771e231460c367508e9689c0684fb7e3611509
Guadalupe Sahagun <lupecsahagun23@yahoo.com>
2020-09-21 02:43:15 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD3HEc2J2TVtUVf5hPs0rQfEMvrkQuk3TQ",
-    authDomain: "clone-4b589.firebaseapp.com",
-    databaseURL: "https://clone-4b589.firebaseio.com",
-    projectId: "clone-4b589",
-    storageBucket: "clone-4b589.appspot.com",
-    messagingSenderId: "424131879704",
-    appId: "1:424131879704:web:bd47155b3dacdecf9df46d",
-    measurementId: "G-PN6EC0ZEC4"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 });
 
 // const db = firebaseApp.firestore();
